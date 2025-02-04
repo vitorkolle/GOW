010d4178f9c820ba0208f2f8f36db228a83fde75
vitorkolle <vitorpaeskolle@gmail.com>
2025-02-04 12:23:52 -0300

    FEAT: Header estruturado

---
+++ GOW/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <>
+      <div className="h-full w-full flex flex-col bg-[url(./assets/gameImg.svg)] bg-cover bg-fixed">
+      <div className="h-full w-full bg-radial from-[#243E58] from-13% via-black via-35% via-black via-65% via-black via-75% to-[#243E58] to-88% opacity-75">
+        
+        {/* Header */}
+        <div className="w-full h-20 flex items-center justify-evenly">
+          <img src={logo} alt="Logo God of War Ragnarok" />
+
+          <nav className='flex w-2/5 justify-evenly'>
+            <p className="text-white"> <a href="">Personagens</a></p>
+            <p className="text-white"> <a href="">Criaturas</a></p>
+            <p className="text-white"> <a href="">Reinos</a></p>
+          </nav>
+
+          <nav className='flex w-1/5 justify-center'>
+          <p className="text-white"> <a href="">Site Oficial</a> | <a href="">Desenvolvedora</a> </p>
+          </nav>
+        </div>
+
+        {/* Main */}
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
