1f54a537e80ff1697c5f20cd33731a2e0b487e26
vitorkolle <vitorpaeskolle@gmail.com>
2025-02-18 11:31:27 -0300

    FIX: Problema no efeito do background resolvido

--- GOW/src/App.tsx
+++ GOW/src/App.tsx
@@ -4,9 +4,7 @@ import logo from './assets/logo.svg'
 function App() {
   return (
     <>
-      <div className="h-screen w-full flex flex-col bg-[url(./assets/gameImg.svg)] bg-cover bg-no-repeat bg-fixed items-between align-center">  
-      <div className="h-full w-full bg-radial from-[#243E58] from-13% via-black via-35% via-black via-65% via-black via-75% to-[#243E58] to-88%">
-
+        <div className="h-screen w-full flex flex-col bg-[url(./assets/gameImg.svg)] bg-cover bg-no-repeat bg-fixed items-between">
 
         {/* Header */}
         <div className="w-full h-1/5 flex items-start justify-evenly mt-2">
@@ -25,13 +23,10 @@ function App() {
         </div>
 
         {/* Main */}
-        <div className="w-5/6 h-contain justify-center items-center bg-red-500 flex flex-col gap-3">
+        <div className="w-full h-contain justify-center items-center bg-red-500 flex flex-col gap-3">
         <h1 className='text-white text-4xl font-[Phudu] font-medium'>O Destino dos Nove Reinos Está em Suas Mãos</h1>
-        <p className='text-white text-xl font-[Lexend] font-light text-center w-1/2'>Enfrente o fim dos tempos em God of War Ragnarök e decida o destino dos Nove Reinos em uma jornada épica e inesquecível.</p>
+        <p className='text-white text-xl font-[Lexend] font-medium text-center w-3/7'>Enfrente o fim dos tempos em God of War Ragnarök e decida o destino dos Nove Reinos em uma jornada épica e inesquecível.</p>
         </div>
-
-
-      </div>
       </div>
     </>
   )
